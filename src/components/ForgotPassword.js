18c779c7641978dee958094793af52bda4914a61
raghava20 <raghavaanand22@gmail.com>
2022-01-19 16:22:03 +0530

    changed classname

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -6,18 +6,18 @@ import image from "../images/img1.png";
 export default function ForgotPassword() {
     return (
         <>
-            <div className="forgotpassword">
+            <div className="forgotpasswordPage">
                 <div className="forgotpassword-logo">
                     <img src={image} alt='' />
                 </div>
                 <form className="forgotpassword-form">
-                    <h2 className="title">Forgot Password</h2>
+                    <h2 className="titles">Forgot Password</h2>
                     <p>Please enter your email address and we'll send you a link to reset your password!</p>
                     <div className="input-field">
                         <i className="fas fa-envelope"></i>
                         <TextField type="text" label="Email" variant="standard" fullWidth />
                     </div>
-                    <button type="submit" className="btn">Send Link</button>
+                    <button type="submit" className="fpBtn">Send Link</button>
                 </form>
             </div>
         </>
