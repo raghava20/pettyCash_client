b11cd2ce62335730f22e97a2f29380d1cfd93953
raghava20 <raghavaanand22@gmail.com>
2022-01-29 11:39:43 +0530

    added useContext hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './components/Login';
 import SignUp from './components/SignUp';
@@ -7,22 +8,29 @@ import ResetPassword from './components/ResetPassword';
 import SideBar from './components/SideBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
-      <Routes>
-        <Route exact path="/dashboard" element={<SideBar />} />
-        <Route exact path="/add-expenses" element={<SideBar />} />
-        <Route exact path="/expenses-list" element={<SideBar />} />
-      </Routes>
+export const PrintContext = React.createContext('')
 
-    </BrowserRouter >
+function App() {
+  const [data, setData] = useState([])
+  return (
+    <PrintContext.Provider value={{ data, setData }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:id" element={<ResetPassword />} />
+        </Routes>
+        <Routes>
+          <Route exact path="/dashboard" element={<SideBar />} />
+          <Route exact path="/add-expenses" element={<SideBar />} />
+          <Route exact path="/expenses-list" element={<SideBar />} />
+          <Route exact path="/transferred-amount" element={<SideBar />} />
+          <Route exact path="/dashboard/print-details" element={<SideBar />} />
+        </Routes>
+
+      </BrowserRouter >
+    </PrintContext.Provider>
   );
 }
 
