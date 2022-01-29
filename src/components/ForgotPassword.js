dc80d1fbbb8c1e78e46c413e31d8332c58d281e9
raghava20 <raghavaanand22@gmail.com>
2022-01-29 13:00:07 +0530

    done with authentication

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,23 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/ForgotPassword.css";
 import { TextField } from "@mui/material";
 import image from "../images/img1.png";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { ErrorMessage } from "./Utils";
 
 export default function ForgotPassword() {
+    const [error, setError] = useState(null)
+    const [disabled, setDisabled] = useState(false)
+
+
+    const onSubmit = async (values) => {
+
+        await fetch("http://localhost:3001/forgot-password", {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: values.email
+            })
+        })
+            .then(data => data.json())
+            .then(data => setError(data.message))
+        if (error === "Email has been sent, kindly follow the instructions.") return setDisabled(true)
+    }
+
+    const { handleBlur, handleChange, handleSubmit, errors, touched, values } = useFormik({
+        initialValues: {
+            email: ""
+        },
+        validationSchema: yup.object({
+            email: yup.string().required("Please enter email!").email("Email must be a valid email!")
+        }),
+        onSubmit
+    })
+
     return (
         <>
             <div className="forgotpasswordPage">
                 <div className="forgotpassword-logo">
                     <img src={image} alt='' />
                 </div>
-                <form className="forgotpassword-form">
+                <div className="error__message">{error ? (<ErrorMessage>{error}</ErrorMessage>) : ""}</div>
+                <form className="forgotpassword-form" onSubmit={handleSubmit}>
                     <h2 className="titles">Forgot Password</h2>
                     <p>Please enter your email address and we'll send you a link to reset your password!</p>
                     <div className="input-field">
                         <i className="fas fa-envelope"></i>
-                        <TextField type="text" label="Email" variant="standard" fullWidth />
+                        <TextField type="text" label="Email" name="email" variant="standard" fullWidth value={values.email} onChange={handleChange} onBlur={handleBlur} />
                     </div>
-                    <button type="submit" className="fpBtn">Send Link</button>
+                    <div className="error__message">{touched.email && errors.email ? (<ErrorMessage>{errors.email}</ErrorMessage>) : null}</div>
+                    <button type="submit" className="fpBtn" disabled={disabled}>Send Link</button>
                 </form>
             </div>
         </>
--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,27 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/ResetPassword.css"
 import { TextField } from "@mui/material";
 import image from "../images/img1.png";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { ErrorMessage } from "./Utils";
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function ResetPassword() {
+    const [error, setError] = useState(null)
+    const [show, setShow] = useState(false)
+    const [disabled, setDisabled] = useState(false)
+    const { id } = useParams();
+
+    const onSubmit = async (values) => {
+        await fetch("http://localhost:3001/reset-password", {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                newPassword: values.newPassword,
+                resetLink: id,
+            })
+        })
+            .then(data => data.json())
+            .then(data => {
+                setError(data.message)
+                if (data.message === 'Your password has been changed successfully!') {
+                    setDisabled(true);
+                    setShow(true)
+                }
+            }
+            )
+
+    }
+
+    const PASSWORD_REGEX = /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/
+    const { handleBlur, handleChange, handleSubmit, errors, touched, values } = useFormik({
+        initialValues: {
+            newPassword: "",
+            confirmPassword: "",
+        },
+        validationSchema: yup.object({
+            newPassword: yup.string().required("Please enter password!").matches(PASSWORD_REGEX,
+                "Password must follow pattern (Aa#1)"
+            ),
+            confirmPassword: yup.string().required("Please confirm your password!").when("newPassword", {
+                is: val => val && val.length > 0 ? true : false,
+                then: yup.string().oneOf([yup.ref("newPassword")], "Password does not match!")
+            })
+        }),
+        onSubmit
+    })
+
     return (
         <>
             <div className="resetpasswordPage">
                 <div className="resetpassword-logo">
                     <img src={image} alt='' />
                 </div>
-                <form className="resetpassword-form">
+                <div className="error__message">{error ? (<ErrorMessage>{error}</ErrorMessage>) : ""}</div>
+                <form className="resetpassword-form" onSubmit={handleSubmit}>
                     <h2 className="title">Reset Your Password</h2>
                     <div className="input-field">
                         <i className="fas fa-envelope"></i>
-                        <TextField type="Password" label="New Password" variant="standard" fullWidth />
+                        <TextField type="Password" label="New Password" name="newPassword" variant="standard" fullWidth onChange={handleChange} onBlur={handleBlur} value={values.newPassword} />
                     </div>
+                    <div className="error__message"> {touched.newPassword && errors.newPassword ? (<ErrorMessage>{errors.newPassword}</ErrorMessage>) : null}</div>
                     <div className="input-field">
                         <i className="fas fa-lock"></i>
-                        <TextField type="password" label="Confirm Your Password" variant="standard" fullWidth />
+                        <TextField type="password" label="Confirm Your Password" name="confirmPassword" variant="standard" fullWidth onChange={handleChange} onBlur={handleBlur} value={values.confirmPassword} />
                     </div>
-                    <button type="submit" className="rpBtn">Save Changes</button>
+                    <div className="error__message">{touched.confirmPassword && errors.confirmPassword ? (<ErrorMessage>{errors.confirmPassword}</ErrorMessage>) : null}</div>
+                    <button type="submit" className="rpBtn" disabled={disabled}>Save Changes</button>
                 </form>
+                {show ? <div className="footer">Sign in to your account: <Link className="links" to='/login'>Sign In</Link></div> : ""}
 
             </div>
         </>
